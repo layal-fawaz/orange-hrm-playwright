d1812e2dbb8e3c94cfdecce5e3f8b6ca6e1b24f4
layal-fawaz <layalfrh2016@gmail.com>
2026-03-08 00:56:42 +0200

    Fix pylint PEP8 findings

--- pages/pim_page.py
+++ pages/pim_page.py
@@ -1,8 +1,9 @@
 class PimPage:
-    """Page object for PIM page"""
+    """Page object for PIM page."""
+
     def __init__(self, page):
-        self.page = page  
+        self.page = page
+
     def go_to_add_employee(self):
         self.page.get_by_role("link", name="PIM").click()
         self.page.get_by_role("link", name="Add Employee").click()
-        
