05a4f1875039bbed9f9d921aff5cbf2b6e947b46
layal-fawaz <layalfrh2016@gmail.com>
2026-03-13 23:49:19 +0200

    Refactor: Implement instructor feedback and improve test stability

--- tests/e2e/test_add_employee.py
+++ tests/e2e/test_add_employee.py
@@ -1,6 +1,6 @@
 def test_add_employee_without_login_details(add_employee_page_fixture):
-    add_employee_page_fixture.add_employee("Layal", "Fawaz", "1234")
+    add_employee_page_fixture.add_employee("Layal", "Fawaz")
 
 def test_add_employee_with_login_details(add_employee_page_fixture):
-    add_employee_page_fixture.add_employee("Layal", "Fawaz", "11123")
+    add_employee_page_fixture.add_employee("Layal", "Fawaz")
     add_employee_page_fixture.enable_login_details("layal12", "22l44ayyiw")
