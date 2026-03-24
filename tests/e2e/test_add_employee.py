1c401c53f6bea0a43c4ccfedd2ed4f984f269d91
layal-fawaz <layalfrh2016@gmail.com>
2026-03-25 00:20:38 +0200

    Add employee page refactor

--- tests/e2e/test_add_employee.py
+++ tests/e2e/test_add_employee.py
@@ -1,6 +1,6 @@
 def test_add_employee_without_login_details(add_employee_page_fixture):
-    add_employee_page_fixture.add_employee("Layal", "Fawaz")
-
+    add_employee_page_fixture.add_employee("Layal", "Fawaz", "Alhusseini")
 def test_add_employee_with_login_details(add_employee_page_fixture):
-    add_employee_page_fixture.add_employee("Layal", "Fawaz")
+    add_employee_page_fixture.add_employee("Layal", "Fawaz","Alhusseini", save=False)
     add_employee_page_fixture.enable_login_details("layal12", "22l44ayyiw")
+
