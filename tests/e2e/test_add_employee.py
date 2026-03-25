cda655cacf2f2d44e4ff36db2df8148dc8b96dae
layal-fawaz <layalfrh2016@gmail.com>
2026-03-25 11:22:57 +0200

    Fix test_add_employee

--- tests/e2e/test_add_employee.py
+++ tests/e2e/test_add_employee.py
@@ -2,5 +2,5 @@ def test_add_employee_without_login_details(add_employee_page_fixture):
     add_employee_page_fixture.add_employee("Layal", "Fawaz", "Alhusseini")
 def test_add_employee_with_login_details(add_employee_page_fixture):
     add_employee_page_fixture.add_employee("Layal", "Fawaz","Alhusseini", save=False)
-    add_employee_page_fixture.enable_login_details("layal12", "22l44ayyiw")
+    add_employee_page_fixture.enable_login_details("layal4", "22l44ayyiw")
 
